d57b41ca9b683c6f5fba5d6a19597a6300be7af3
HanamiYuzu <txw5ifd9@duck.com>
2024-12-03 15:15:17 +0800

    day2 puzzle2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,24 +1,50 @@
+use itertools::Itertools;
+
 fn main() {
     let str = include_str!("../input.txt");
     puzzle1(str);
+    puzzle2(str);
 }
 
 fn puzzle1(str: &str) -> i32 {
     let input = parse(str);
     let count = input
         .iter()
-        .map(|v| {
-            let d = v.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
-            match d[0].signum() {
-                0 => false,
-                1 => d.iter().all(|&x| x >= 1 && x <= 3),
-                _ => d.iter().all(|&x| x >= -3 && x <= -1),
-            }
-        })
+        .map(|v| is_safe(v.iter()))
         .map(|x| x as i32)
         .sum();
     println!("puzzle1: {count}");
     count
+}
+
+fn puzzle2(str: &str) -> i32 {
+    let input = parse(str);
+    let count = input
+        .iter()
+        .map(|v| {
+            v.iter()
+                .enumerate()
+                .any(|(i, _)| is_safe(v[0..i].iter().chain(v[i + 1..].iter())))
+        })
+        .map(|x| x as i32)
+        .sum();
+    println!("puzzle2: {count}");
+    count
+}
+
+fn is_safe<'a, T>(iter: T) -> bool
+where
+    T: Iterator<Item = &'a i32>,
+{
+    let d = iter
+        .tuple_windows::<(_, _)>()
+        .map(|(a, b)| b - a)
+        .collect::<Vec<_>>();
+    match d[0].signum() {
+        0 => false,
+        1 => d.iter().all(|&x| x >= 1 && x <= 3),
+        _ => d.iter().all(|&x| x >= -3 && x <= -1),
+    }
 }
 
 fn parse(str: &str) -> Vec<Vec<i32>> {
@@ -44,5 +70,16 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9";
         assert_eq!(puzzle1(input), 2);
+    }
+
+    #[test]
+    fn test_puzzle2() {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!(puzzle2(input), 4);
     }
 }
