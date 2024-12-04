884f86fb00e77af67647f93538e3662c1b119ff1
HanamiYuzu <txw5ifd9@duck.com>
2024-12-04 15:33:37 +0800

    day3 puzzle2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 fn main() {
     let str = include_str!("../input.txt");
     puzzle1(str);
+    puzzle2(str);
 }
 
 fn puzzle1(str: &str) -> i32 {
@@ -14,6 +15,18 @@ fn puzzle1(str: &str) -> i32 {
         .map(|(_, [a, b])| a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap())
         .sum();
     println!("puzzle1 = {}", sum);
+    sum
+}
+
+fn puzzle2(str: &str) -> i32 {
+    let mut str = parse(str);
+    let mut sum = 0;
+    while let Some(pos) = str.find("don't()") {
+        sum += puzzle1(&str[..pos]);
+        str = str[pos..].find("do()").map_or("", |pp| &str[pos + pp..]);
+    }
+    sum += puzzle1(str);
+    println!("puzzle2 = {}", sum);
     sum
 }
 
@@ -29,5 +42,11 @@ mod tests {
     fn test_puzzle1() {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(puzzle1(input), 161);
+    }
+
+    #[test]
+    fn test_puzzle2() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(puzzle2(input), 48);
     }
 }
