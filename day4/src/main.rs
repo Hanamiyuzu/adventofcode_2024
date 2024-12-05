d4bc9b7d2450884b1006f0c05c01d119bd69cc4a
HanamiYuzu <txw5ifd9@duck.com>
2024-12-05 16:32:54 +0800

    day 4 puzzle 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let str = include_str!("../input.txt");
     puzzle1(str);
+    puzzle2(str);
 }
 
 fn puzzle1(str: &str) -> i32 {
@@ -24,6 +25,31 @@ fn puzzle1(str: &str) -> i32 {
     count
 }
 
+fn puzzle2(str: &str) -> i32 {
+    let map = parse(str);
+    let count = map
+        .iter()
+        .enumerate()
+        .map(|(i, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, &ch)| ch == 'A')
+                .map(|(j, _)| {
+                    let a = safe_get(&map, i as i32 - 1, j as i32 - 1);
+                    let b = safe_get(&map, i as i32 + 1, j as i32 + 1);
+                    let c = safe_get(&map, i as i32 + 1, j as i32 - 1);
+                    let d = safe_get(&map, i as i32 - 1, j as i32 + 1);
+                    ((a == 'M' && b == 'S') || (a == 'S' && b == 'M'))
+                        && ((c == 'M' && d == 'S') || (c == 'S' && d == 'M'))
+                })
+                .map(|x| x as i32)
+                .sum::<i32>()
+        })
+        .sum();
+    println!("puzzle2 = {}", count);
+    count
+}
+
 const DIRS: [(i32, i32); 8] = [
     (-1, -1),
     (-1, 0),
@@ -36,10 +62,7 @@ const DIRS: [(i32, i32); 8] = [
 ];
 
 fn search(map: &Vec<Vec<char>>, i: i32, j: i32, ch: char, dir: &(i32, i32)) -> i32 {
-    if i < 0 || i >= map.len() as i32 || j < 0 || j >= map[0].len() as i32 {
-        return 0;
-    }
-    if map[i as usize][j as usize] != ch {
+    if safe_get(map, i, j) != ch {
         return 0;
     }
     match ch {
@@ -49,6 +72,13 @@ fn search(map: &Vec<Vec<char>>, i: i32, j: i32, ch: char, dir: &(i32, i32)) -> i
         'A' => search(map, i + dir.0, j + dir.1, 'S', dir),
         _ => 0,
     }
+}
+
+fn safe_get(map: &Vec<Vec<char>>, i: i32, j: i32) -> char {
+    if i < 0 || i >= map.len() as i32 || j < 0 || j >= map[0].len() as i32 {
+        return '.';
+    }
+    map[i as usize][j as usize]
 }
 
 fn parse(str: &str) -> Vec<Vec<char>> {
@@ -72,5 +102,20 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
         assert_eq!(puzzle1(input), 18);
+    }
+
+    #[test]
+    fn test_puzzle2() {
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!(puzzle2(input), 9);
     }
 }
