f40abce7942c0918f491892d65621824460dd1ec
HanamiYuzu <txw5ifd9@duck.com>
2024-12-06 10:19:42 +0800

    day 5 puzzle 1

---
+++ day5/src/main.rs
@@ -0,0 +1,77 @@
+fn main() {
+    let str = include_str!("../input.txt");
+    puzzle1(str);
+}
+
+fn puzzle1(str: &str) -> usize {
+    let (rules, input) = parse(str);
+    let sum = input
+        .iter()
+        .map(|v| {
+            for (i, &x) in v.iter().enumerate() {
+                for &y in v.iter().skip(i + 1) {
+                    if rules[y][x] {
+                        return 0;
+                    }
+                }
+            }
+            v[v.len() / 2]
+        })
+        .sum();
+    println!("puzzle1 = {}", sum);
+    sum
+}
+
+fn parse(str: &str) -> ([[bool; 100]; 100], Vec<Vec<usize>>) {
+    let (s1, s2) = str.split_once("\n\n").unwrap();
+    let mut rules = [[false; 100]; 100];
+    s1.lines().for_each(|s| {
+        let (a, b) = s.split_once('|').unwrap();
+        let a = a.parse::<usize>().unwrap();
+        let b = b.parse::<usize>().unwrap();
+        rules[a][b] = true;
+    });
+    let input = s2
+        .lines()
+        .map(|s| s.split(',').map(|x| x.parse::<usize>().unwrap()).collect())
+        .collect();
+    (rules, input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_puzzle1() {
+        let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+        assert_eq!(puzzle1(input), 143);
+    }
+}
