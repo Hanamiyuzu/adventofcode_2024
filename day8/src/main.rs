5834b291fb0a8e0bdd8a6e5d41ad37dfd8328a9f
HanamiYuzu <txw5ifd9@duck.com>
2024-12-11 17:41:54 +0800

    day 8 puzzle 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::{HashMap, HashSet};
 fn main() {
     let str = include_str!("../input.txt");
     println!("puzzle1 = {}", puzzle1(str));
+    println!("puzzle2 = {}", puzzle2(str));
 }
 
 fn puzzle1(str: &str) -> usize {
@@ -30,6 +31,32 @@ fn puzzle1(str: &str) -> usize {
     }
     ans.len()
 }
+
+fn puzzle2(str: &str) -> usize {
+    let map = parse(str);
+    let (rows, cols) = (map.len() as i32, map[0].len() as i32);
+    let mut antennas = HashMap::new();
+    for (i, row) in map.iter().enumerate() {
+        for (j, &ch) in row.iter().enumerate().filter(|(_, &ch)| ch != '.') {
+            antennas
+                .entry(ch)
+                .or_insert(Vec::new())
+                .push((i as i32, j as i32));
+        }
+    }
+    let mut ans = HashSet::new();
+    for v in antennas.values() {
+        for perm in v.iter().permutations(2) {
+            let (dr, dc) = (perm[1].0 - perm[0].0, perm[1].1 - perm[0].1);
+            let mut row = perm[0].0;
+            let mut col = perm[0].1;
+            loop {
+                if row < 0 || row >= rows || col < 0 || col >= cols {
+                    break;
+                }
+                ans.insert((row, col));
+                row -= dr;
+                col -= dc;
             }
         }
     }
@@ -59,5 +86,22 @@ mod tests {
 ............
 ............";
         assert_eq!(puzzle1(input), 14);
+    }
+
+    #[test]
+    fn test_puzzle2() {
+        let input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+        assert_eq!(puzzle2(input), 34);
     }
 }
